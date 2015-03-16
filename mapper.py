83b40b05ec4bf175c69625d327c50dced63e0603
Tom Pollard <tom.pollard.11@ucl.ac.uk>
2015-03-16 16:59:54 +0000

    bug in mapper

--- mapper.py
+++ mapper.py
@@ -3,18 +3,10 @@
 import sys
 import re
 
-def mapper(argv): 
+def mapper(stream): 
     pattern = re.compile('[a-zA-Z][a-zA-Z0-9]*') 
-    for line in sys.stdin: 
+    for line in stream: 
         for word in pattern.findall(line): 
             print word.lower() + '\t' + '1'
-
-# def mapper(inputdata):
-#     for line in inputdata:
-#         line = line.strip()
-#         unpacked = line.split(",")
-#         time, variable, value = line.split(",")
-#         results = [variable, "1"]
-#         print("\t".join(results))
 
 mapper(sys.stdin)
