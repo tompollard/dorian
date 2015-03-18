fa5e7daeb30d346e3867f3ffe5136bb50ff0338a
Tom Pollard <tom.pollard.11@ucl.ac.uk>
2015-03-18 17:37:44 +0000

    update shell script

--- runme.sh
+++ runme.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
+# Script that counts the number of words in a book
 
-# Save wordcount
-OUTPUTFILE="output/counted.txt"
-cat dorian.txt | ./mapper.py | sort | ./reducer.py > $OUTPUTFILE
+# Has a filename for the book been provided?
+thebook="$1"
+if [ -z "$thebook" ] ; then
+    echo "Please provide the filename of the book."
+    exit 1
+elif [ ! -f "$thebook" ] ; then
+    echo "$thebook was not found."
+    exit 1
+fi
+
+# Create output directory
+mkdir -p "output"
+
+# Count the words and output the results
+OUTPUTFILE="output/wordcount.txt"
+cat $thebook | ./mapper.py | sort | ./reducer.py > $OUTPUTFILE
+echo "Word count saved as $OUTPUTFILE" 
 
